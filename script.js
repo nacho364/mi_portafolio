5f8a7ed031ec941b277137ffae49b86433af06f1
nacho <nachoskere3@gmail.com>
2026-07-01 12:36:57 -0300

    feat: agregar funciones para modificar titulo

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function saludar() {
     alert("¡Gracias por visitar mi portfolio!");
 }
+function cambiarTitulo() {
+    document.getElementById("titulo").innerHTML = "¡Gracias por visitar mi portfolio!";
+}function restaurarTitulo() {
+    document.getElementById("titulo").innerHTML = "Portafolio de Ignacio";
+}
